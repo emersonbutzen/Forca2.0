1e8feefdd7b260ea03d849d5b002bdc0874fec8f
emerson.butzen <emerson@butzen.com.br>
2021-11-12 00:49:32 +0000

    feature(classes): Updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { Jogo } from './jogo.js';
+
 const keys = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Ç','Z','X','C','V','B','N','M',]
 
 const finalWord = 'teste'
--- src/jogo.js
+++ src/jogo.js
@@ -1,3 +1,4 @@
+import { Jogador } from './jogador.js';
 class Jogo {
     #palavras = [];
     #palavraSecreta;
